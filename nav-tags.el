f328b0234e104a91cdfd943f956dbc9c3f6e14cf
Issac Trotts <issac.trotts@gmail.com>
2009-08-11 20:37:14 -0700

    Refactor nav-tags.el, replacing setqs with lets.

--- nav-tags.el
+++ nav-tags.el
@@ -24,23 +24,37 @@
 
 (setq nav-tags-mode-map (nav-tags-make-mode-map))
 
-;; copied imenu internal functions to handle sort.
+;; Copied imenu internal functions to handle sort.
 (defun imenu--sort-by-name (item1 item2)
   (string-lessp (car item1) (car item2)))
 
 
-(defun nav-tags-fetch-imenu (file)
+(defun nav-tags-fetch-imenu (filename)
   "Generates the tag index from selected file."
   (require 'imenu)
-  (setq nav-tags-filename file)
-  (nav-open-file file)
-  (setq imenu--index-alist nil)
+  (setq nav-tags-filename filename)
+  (nav-open-file filename)
   (setq index-alist (imenu--make-index-alist t))
   (if imenu-sort-function
       (setq index-alist (sort index-alist imenu-sort-function)))
   (set-buffer nav-buffer-name)
   (select-window (nav-get-window nav-buffer-name))
   (nav-tags))
+
+
+(defun nav-jump-to-tag-of-button (button)
+  ;; This first select-window seems not to do anything. Can we remove it?
+  (select-window (nav-get-window nav-buffer-name))
+
+  (let* ((num (replace-regexp-in-string "^.* \\[" "" 
+					(button-label button)))
+	 (num (substring num 0 -1)))
+    (select-window (nav-get-window nav-tags-filename))
+    (goto-char (string-to-number num)))
+
+  ;; recenter-top-bottom is not defined in emacs 22.
+  (when (functionp 'recenter-top-bottom)
+      (recenter-top-bottom)))
 
 
 (defun nav-tags-show-tags ()
@@ -50,29 +64,17 @@
     (erase-buffer)
     (insert (nav-make-header nav-tags-filename))
     (insert "\n")
-    (setq nav-vars (cdr (cdr (cdr index-alist))))
-    (while nav-vars
-      (setq nav-tag-fun (car nav-vars))
-      (setq nav-tag-text (car nav-tag-fun))
-      (setq nav-tag-marker (cdr nav-tag-fun))
-      (setq nav-tag-buffer (marker-buffer nav-tag-marker))
-      (setq nav-tag-position (marker-position nav-tag-marker))
-      ;make button here
-      (setq nav-tag-button-text (concat nav-tag-text " [" (number-to-string nav-tag-position) "]"))
-      (insert-button nav-tag-button-text
-		     'action (lambda (button)
-			       (select-window (nav-get-window nav-buffer-name))
-			       (setq num (replace-regexp-in-string "^.* \\[" "" 
-								   (button-label button)))
-			       (setq num (substring num 0 -1))
-			       (select-window (nav-get-window nav-tags-filename))
-			       (goto-char (string-to-number num))
-			       (recenter-top-bottom))
-		     'follow-link t
-		     'face nav-button-face
-		     'help-echo nil)
-      (insert "\n")
-      (setq nav-vars (cdr nav-vars)))
+    (dolist (tag (cdddr index-alist))
+      (let* ((tag-name (car tag))
+	     (tag-marker (cdr tag))
+	     (tag-position (marker-position tag-marker))
+	     (button-text (concat tag-name " [" (number-to-string tag-position) "]")))
+	(insert-button button-text
+		       'action 'nav-jump-to-tag-of-button
+		       'follow-link t
+		       'face nav-button-face
+		       'help-echo nil)
+	(insert "\n")))
     (setq mode-line-format "nav: Tag list")
     (force-mode-line-update)
     (setq truncate-lines t)
