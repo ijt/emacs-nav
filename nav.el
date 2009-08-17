2d48ac646afef4f13ef52ea1af9077b766700e0c
matthew.ozor <matthew.ozor@gmail.com>
2009-08-17 14:52:28 -0400

    Buf and tag mode removes timer hook.

--- nav.el
+++ nav.el
@@ -72,7 +72,7 @@
   :type '(repeat string)
   :group 'nav)
 
-(defcustom nav-follow t
+(defcustom nav-follow nil
   "*If t, nav will follow buffer's directory."
   :type 'boolean
   :group 'nav)
@@ -273,7 +273,7 @@ This is used if only one window besides the Nav window is visible."
 
 (defun nav-show-bufs ()
   (interactive)
-  (if nav-follow (cancel-timer nav-timer))
+  (if nav-follow (setq window-configuration-change-hook nil))
   (nav-bufs))
 
 
@@ -798,7 +798,7 @@ http://muffinresearch.co.uk/archives/2007/01/30/bash-single-quotes-inside-of-sin
 (defun nav-tags-expand ()
   "Shows all function tags in file."
   (interactive)
-  (if nav-follow (cancel-timer nav-timer))
+  (if nav-follow (setq window-configuration-change-hook nil))
   (nav-save-cursor-line)
   (nav-tags-fetch-imenu (nav-get-cur-line-str)))
 
