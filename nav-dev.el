8ea35c0e8abea2e44c08b40b9596a46397ccc224
Issac Trotts <issac.trotts@gmail.com>
2012-05-06 22:45:13 -0700

    Remove defunct keybinding

--- nav-dev.el
+++ nav-dev.el
@@ -1,14 +1,10 @@
-;;; nav-dev.el --- Key bindings etc. to help develop nav
+;;; nav-dev.el --- Functions to help develop nav
 ;;
+
 ;; Copyright 2009 Google Inc. All Rights Reserved.
 ;;
 ;; Author: issactrotts@google.com (Issac Trotts)
 ;;
-
-;; f7 runs nav's tests
-(define-key emacs-lisp-mode-map [f7] (lambda ()
-                                       (interactive)
-                                       (load-file "nav-test.el")))
 
 (defun byte-compile-current-file ()
   (interactive)
